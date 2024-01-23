4bc25880ed0531d19f743c0f2de915735bc182a6
Eszter <szteran@gmail.com>
2024-01-23 13:35:02 +0100

    added register, session storage,  logout, redirect

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,40 +1,58 @@
 import { useState } from "react"
 import { app } from "../firebase-config"
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from "react-router-dom"
 
 function Form({ title }) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate()
 
     const handleAction = () => {
-
         const auth = getAuth(app);
 
         if(title === "login") {
-            console.log("login: ", email, password)
+            signInWithEmailAndPassword(auth, email, password)
+            .then(res => {
+                sessionStorage.setItem('token', res._tokenResponse.refreshToken)
+                navigate('/')
+            })
         } else if (title === "register") {
-            console.log("register: ", email, password)
-
             createUserWithEmailAndPassword(auth, email, password)
             .then(res => {
-                console.log(res)
                 sessionStorage.setItem('token', res._tokenResponse.refreshToken)
+				navigate('/')
             })
         }
     }
 
 
   return (
-      <form>
-        <h3>{title} form</h3>
+    <>
+        <form>
+            <h3>{title} form</h3>
 
-        <input type="text" autoComplete="off" placeholder="e-mail" onChange={event => setEmail(event.target.value)}/>
-        {/* autoComplete - off: így nem akarja automatikusan kitölteni a korábban már beírt adatokat. Pl. a jelszónál. Ez jó lehet, de ost gyakorlunk, és hülyeségeket írunk csak bele */}
+            <input type="text" autoComplete="off" placeholder="e-mail" onChange={event => setEmail(event.target.value)} />
+            {/* autoComplete - off: így nem akarja automatikusan kitölteni a korábban már beírt adatokat. Pl. a jelszónál. Ez jó lehet, de ost gyakorlunk, és hülyeségeket írunk csak bele */}
 
-        <input type="password" autoComplete="off" placeholder="password" onChange={event => setPassword(event.target.value)}/>
+            <input type="password" autoComplete="off" placeholder="password" onChange={event => setPassword(event.target.value)} />
 
-        <button type="button" onClick={handleAction}>{title}</button>
-    </form>
+            <button type="button" onClick={handleAction}>{title}</button>
+        </form>
+
+      {/*   <button onClick={() => {
+            if (title === "login") navigate('/register')
+            else navigate('/login')
+        }}>{title === "login" ? "go to registration" : "go to login"}</button> */}
+
+        {title === "login"
+            ?
+            <button onClick={() => navigate('/register')}>go to register</button>
+            :
+            <button onClick={() => navigate('/login')}>go to login</button>
+        }
+        
+    </>
   )
 }
 
